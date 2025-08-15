8be8bb3af9cae8db23d716beb4afe1390977ab5b
최지혜 <choejihye@choejihyeui-MacBookPro.local>
2025-08-16 03:23:49 +0900

    d

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// 프로덕션 환경에서는 모든 console 로그( log / warn / error )를 무시하도록 오버라이드합니다.
-if (import.meta.env.PROD) {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const noop = () => {};
-  // eslint-disable-next-line no-console
-  console.log = noop;
-  // eslint-disable-next-line no-console
-  console.warn = noop;
-  // eslint-disable-next-line no-console
-  console.error = noop;
-}
+// 프로덕션 환경에서도 로그가 보이도록 console 오버라이드 제거
+// if (import.meta.env.PROD) {
+//   // eslint-disable-next-line @typescript-eslint/no-empty-function
+//   const noop = () => {};
+//   // eslint-disable-next-line no-console
+//   console.log = noop;
+//   // eslint-disable-next-line no-console
+//   console.warn = noop;
+//   // eslint-disable-next-line no-console
+//   console.error = noop;
+// }
 
 createRoot(document.getElementById("root")!).render(<App />);
